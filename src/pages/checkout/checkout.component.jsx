b3c67870c41442b58bddb6ce117f67a9645e57f9
GitnGeek <5278234+GitnGeek@users.noreply.github.com>
2022-07-13 12:56:57 +0200

    updating stripe integration and favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import {
   selectCartItems,
@@ -35,6 +37,13 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className='total'>
       <span>TOTAL: ${total}</span>
     </div>
+    <div className='test-warning'>
+      *Please use the folowing test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 12/34 - CVV: 123
+      <br />
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
